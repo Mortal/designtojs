b3355b13dba1a2d48a9a5ae9319b5b42e728ef95
binlain <lainex@gmx.de>
2014-08-16 08:03:49 +0200

    fix trailing newline

--- design.js
+++ design.js
@@ -1,6 +1,11 @@
 
 var compile = require('./lib/compile.js');
+var fs = require('fs');
 
 compile.compileFile(process.argv[2], function(error, output){
-    console.log(output); 
+    if(!process.argv[3]){
+        process.stdout.write(output); 
+    } else {
+        fs.writeFileSync(process.argv[3], output); 
+    }
 });
